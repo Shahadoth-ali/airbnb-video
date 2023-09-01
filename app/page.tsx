9ffe578bac04901711dd68b8ce3895ccef353adf
Shahadoth-ali <md.sahadat.live@gmail.com>
2023-09-01 20:46:33 +0600

    fith commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
 
 
+export const dynamic = 'force-dynamic';
+
 
 interface HomeProps{
   searchParams:IListingsParams
